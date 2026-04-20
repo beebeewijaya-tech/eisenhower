ccd5dd2981ff569c010aeb9f72fc3304b63edc8e
Bee <beebeewijaya.tech@gmail.com>
2026-04-20 20:53:38 +0700

    init: initializing project and add structure project
    -- What
    
    works on project structuring and adding bunch of colors, also add several important file
    
    -- Why
    
    need to make sure the project following MVVM architecture

---
+++ Eisenhower/EisenhowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EisenhowerApp.swift
+//  Eisenhower
+//
+//  Created by Bee Wijaya on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct EisenhowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Eisenhower/Screen/Register/RegisterScreen.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterScreen.swift
+//  Eisenhower
+//
+//  Created by Bee Wijaya on 21/04/26.
+//
+
+import SwiftUI
+
+struct RegisterScreen: View {
+    // TODO: works on register screen
+    var body: some View {
+        Text("Register Screen")
+    }
+}
+
+#Preview {
+    RegisterScreen()
+}
